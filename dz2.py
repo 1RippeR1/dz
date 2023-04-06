7e485164eb4f4468502540efd1b0bf513d2e46a1
Maxim <pupers38@gmail.com>
2023-04-06 16:18:50 +0300

    Dz 06.04.2023

---
+++ dz2.py
@@ -0,0 +1,21 @@
+# cortej = (True,123,'fdsdfs')
+# slovar = {}
+# for i in cortej:
+#     a = type(i)
+#     slovar[a] = i
+# print(slovar)
+
+# spisok = ['qweq','qweq','ewq','ewq']
+# temp = []
+# for i in spisok:
+#     if i not in temp:
+#         temp.append(i)
+# print(temp)
+
+# spisok = [1,2,3,4,5]
+# spisok2 = []
+# for i in spisok:
+#     if i % 2 == 1:
+#         spisok2.append(i)
+# print(spisok2)
+
